bea4a1511b706598614eea3ac20cfaeecbbb197b
Matthew Gowans <matthewgowans@gmail.com>
2025-04-22 10:37:39 -0600

    fix: Restore styling configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/disc-golf-journey',
+  ...(process.env.NODE_ENV === 'production' ? {
+    output: 'export',
+    basePath: '/disc-golf-journey',
+  } : {}),
   images: {
     unoptimized: true,
   },
