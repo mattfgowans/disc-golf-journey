23e20d2d6d456d1fed6e292ab3b3ddd556b05a12
Matthew Gowans <matthewgowans@216e-matthewgowans.alpinedistrict.org>
2026-02-17 13:40:13 -0700

    Fix GitHub Pages routing config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
+const BASE_PATH = '/disc-golf-journey';
+
 const nextConfig = {
   ...(process.env.NODE_ENV === 'production' ? {
     output: 'export',
-    basePath: '/disc-golf-journey',
+    basePath: BASE_PATH,
+    assetPrefix: `${BASE_PATH}/`,
+    trailingSlash: true,
   } : {}),
   images: {
     unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
