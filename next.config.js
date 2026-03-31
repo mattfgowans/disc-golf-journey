147c762baaf515327d5216fd394147cedfa0b555
Matthew Gowans <matthewgowans@216e-matthewgowans.alpinedistrict.org>
2026-03-31 12:40:44 -0600

    remove static export config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isGithub = process.env.DEPLOY_TARGET === "github";
-
 const nextConfig = {
-  output: "standalone",
-  basePath: isGithub ? "/disc-golf-journey" : "",
-  assetPrefix: isGithub ? "/disc-golf-journey/" : "",
-  trailingSlash: false,
   images: { unoptimized: true },
-  experimental: { appDir: true, serverComponentsExternalPackages: [] },
-  typescript: { ignoreBuildErrors: false },
 };
 
 module.exports = nextConfig;
